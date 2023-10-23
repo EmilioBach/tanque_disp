1d5f40731a602c021fdb54ef408e64597033ab5c
EmilioBach <112587950+EmilioBach@users.noreply.github.com>
2023-10-22 22:05:57 -0300

    Update Disparar.cpp
    last change

--- Disparar.cpp
+++ Disparar.cpp
@@ -13,24 +13,38 @@ void infodisp(int _P, int _N, int _lapiz, int _n_lapiz, int _elastico, int _n_el
   n_lapiz = _n_lapiz;
   elastico = _elastico;
   n_elastico = _n_elastico;
+
+  pinMode(P, OUTPUT);
+  pinMode(N, OUTPUT);
   
+  pinMode(lapiz, OUTPUT);
+  pinMode(n_lapiz, OUTPUT);
+  
+  pinMode(elastico, OUTPUT);
+  pinMode(n_elastico, OUTPUT);
 }
 
 void Disparar::torreta(){
-  
+  digitalWrite(P, LOW);
+	digitalWrite(N, HIGH);
 }
 void Disparar::in_torreta(){
-  
+  digitalWrite(N, LOW);
+  digitalWrite(P, HIGH);
 }
 void Disparar::disp_lapiz(){
-  
+  digitalWrite(lapiz, HIGH);
+  digitalWrite(n_lapiz, LOW);
 }
 void Disparar::stop_pencil(){
-  
+  digitalWrite(lapiz, LOW);
+  digitalWrite(n_lapiz, LOW);
 }
 void Disparar::disp_elsatico(){
-  
+  digitalWrite(elastico, HIGH);
+  digitalWrite(n_elastico, LOW);
 }
 void Disparar::stop_elsatico(){
-  
+  digitalWrite(n_elastico, LOW);
+  digitalWrite(elastico, LOW);
 }
